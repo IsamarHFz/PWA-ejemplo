4e094fd185558393ca784b85c29857ea72ec1de7
Isabel <marisahfz07@gmail.com>
2025-10-08 12:48:16 -0600

    Ajuste de rutas

--- sw.js
+++ sw.js
@@ -43,7 +43,7 @@ self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request).then(response => {
             return response || fetch(event.request).catch(
-                () => caches.match.match("offline.html"));
+                () => caches.match.match(`${BASE_PATH}offline.html`));
         })
     );
 });
